a4ab82c75ebb384d0e8f8beb574decdb7fb2d34c
NQH118 <ngoquanghuy118@.com>
2025-11-16 20:34:43 +0700

    pushcode

--- routes/index.php
+++ routes/index.php
@@ -9,11 +9,7 @@
 // Cú Pháp Điều hướng
 // VD: Điều Đến Trang index.php
 // http://localhost/Nh%C3%B3m%202_WD20307/?action=index
-<<<<<<< HEAD
-=======
-require_once './controllers/Huy/QHcontroller.php';
 
->>>>>>> 414e4fd (first commit)
 $action = $_GET['action'] ?? '/';
 $id = $_GET['id'] ?? '/';
 match ($action) {
@@ -66,17 +62,6 @@ match ($action) {
     'nhacungcap-insert' => (new nhacungcap_contro)->insert_nhacungcap(),
     'nhacungcap-update' => (new nhacungcap_contro)->update_nhacungcap($id),
     'nhacungcap-delete' => (new nhacungcap_contro)->delete_nhacungcap($id),
-    
-<<<<<<< HEAD
-=======
-    // Quản Lý Users
-    'listUsers' => (new QHController)->listUsers(),
-    'createUsers' => (new QHController)->createUsers(),
-    'storeUsers' => (new QHController)->storeUsers(),
-    'editUsers' => (new QHController)->editUsers($id),
-    'updateUsers' => (new QHController)->updateUsers($id),
-    'deleteUsers' => (new QHController)->deleteUsers($id),
-
-    
->>>>>>> 414e4fd (first commit)
 };
+    
+?>
