46625fdc9c5bc1d8ec196cdaa90b9aedebf22561
Nguyen Tai Duc Anh <ducanh234@gmail.com>
2025-11-17 12:32:00 +0700

    'update_mainlan3'

--- views/admin/Tour/update_tuor.php
+++ views/admin/Tour/update_tuor.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="views/admin/assets/style/LayoutCSS/cssCRUD.css" />
+    <link rel="stylesheet" href="views/admin/cssCRUD.css" />
   </head>
   <body>
     <h1>Cập Nhật Tuor</h1>
@@ -39,7 +39,7 @@
       
       <div>
         <span>Nhập Phiên BẢn:</span>
-        <select name="phienban_id" id="">
+        <select name="phienban_id" id="phienban_id">
           <?php
           foreach($arr_phienban as $e){
             if($e->id == $arr_find->phienban_id){
@@ -57,8 +57,46 @@
         </select>
       </div>
       <div>
+        <span>Giá:</span>
+        <input type="text" id="phienban_price" />
+      </div>
+      <div>
+        <span>Thời Gian:</span>
+        <input type="text" id="phienban_thoigian" />
+      </div>
+      <div>
+        <span>Phương Tiện:</span>
+        <input type="text" id="phienban_phuongtien" />
+      </div>
+      <div>
+        <span>Khởi Hành:</span>
+        <input type="text" id="phienban_khoihanh" />
+      </div>
+      <div>
         <button type="submit" name="nut">OK</button>
       </div>
     </form>
+    <script>
+    const selectPhienBan = document.getElementById("phienban_id");
+    const arr_phienban = <?= json_encode($arr_phienban) ?>;
+    selectPhienBan.addEventListener("change", function () {
+        const id = this.value; 
+        const pb = arr_phienban.find(item => item.id == id);
+        if (pb) {
+            document.getElementById("phienban_price").value = pb.price;
+            document.getElementById("phienban_thoigian").value = pb.thoigian;
+            document.getElementById("phienban_phuongtien").value = pb.phuongtien;
+            document.getElementById("phienban_khoihanh").value = pb.khoihanh;
+        } else {
+            document.getElementById("phienban_price").value = "";
+            document.getElementById("phienban_thoigian").value = "";
+            document.getElementById("phienban_phuongtien").value = "";
+            document.getElementById("phienban_khoihanh").value = "";
+        }
+    });
+    window.addEventListener("DOMContentLoaded", () => {
+    phienban_id.dispatchEvent(new Event("change"));
+  });
+</script>
   </body>
 </html>
